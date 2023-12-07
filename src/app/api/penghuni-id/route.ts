e027190d5cc29f82e9bae12355e183e3dca4a94d
auvarifqi <88789984+auvarifqi@users.noreply.github.com>
2023-12-07 18:36:46 +0700

    update api penghuni by id

--- src/app/api/penghuni-id/route.ts
+++ src/app/api/penghuni-id/route.ts
@@ -21,7 +21,7 @@ export async function GET(req: any) {
 
     const penghuniByID = await prisma.user.findUnique({
         where: {
-            ID: req
+            userID: req
         },
     });
 
