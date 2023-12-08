1caada857bc37303174c4709c5bb1099dc26c2e3
Marvel Subekti <18221058@std.stei.itb.ac.id>
2023-12-09 02:48:21 +0700

    Navigation Bar admin dan Page Admin

--- src/app/(pages)/admin-dashboard/page.tsx
+++ src/app/(pages)/admin-dashboard/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 import React from "react";
-import NavigationBar from "../../../components/NavigationBar/NavigationBar";
+import NavigationBarAdmin from "../../../components/NavigationBarAdmin/NavigationBarAdmin";
 
 import Footer from "../../../components/Footer/Footer";
 
 const page = () => {
   return (
     <div>
-      <NavigationBar />
-
+      <NavigationBarAdmin/>
+        <main className="w-full h-full bg-white">
+            <h1 className="text-[#11406A] text-2xl font-bold">Hi, Admin!</h1>
+            <p className="text-[#11406A]">Ready to clean the rooms?</p>
+        </main>
       <Footer />
     </div>
   );
